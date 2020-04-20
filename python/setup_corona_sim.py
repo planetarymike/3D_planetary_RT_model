1bd024bb4735729a9eb3e259ba1e80aa0b6217cd
Mike Chaffin <michael.s.chaffin@gmail.com>
2020-04-20 14:11:51 -0600

    changed build options for speed

--- python/setup_corona_sim.py
+++ python/setup_corona_sim.py
@@ -1,18 +1,24 @@
 # setup_multi.py --- setup routine for wrapping C++ H corona code
 
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize(Extension(
-    "py_corona_sim",
-    ["py_corona_sim.pyx"],      # our Cython source
-    language="c++",             # generate C++ code
-    extra_compile_args=["-std=c++17","-O2","-ftree-vectorize","-march=native"],
-    extra_link_args=["-lm","-fPIC"],
-#    define_macros=[('SRCFNSLOC','"./source_functions/"')],
-    include_dirs=["../bin/",
-                  "/home/mike/Documents/Utilities/boost_1_72_0/",
-                  "/home/mike/Documents/Utilities/eigen-3.3.7/"]))
-#    extra_objects=["/required_procedures/ipbackgroundCFR_fun.o"]))
-)
+extension = Extension("py_corona_sim",
+                      ["py_corona_sim.pyx"],      # our Cython source
+                      language="c++",             # generate C++ code
+                      extra_compile_args=["-std=c++17","-O3",
+                                          "-ftree-vectorize","-march=native",
+                                          "-ffast-math","-funsafe-math-optimizations","-mfpmath=sse",
+                                          "-DNDEBUG"],
+                      extra_link_args=["-lm","-fPIC","-fopenmp"],
+                      include_dirs=["../bin/",
+                                    "/home/mike/Documents/Utilities/boost_1_72_0/",
+                                    "/home/mike/Documents/Utilities/eigen-3.3.7/"])
+
+#    extra_objects=["/required_procedures/ipbackgroundCFR_fun.o"]
+
+setup(name="py_corona_sim",
+      ext_modules = cythonize(extension),
+      )
+
+
