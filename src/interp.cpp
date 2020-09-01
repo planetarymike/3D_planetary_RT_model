a78a7e319e25f20bfbc2788e7dc76723c0850a58
Mike <michael.s.chaffin@gmail.com>
2020-09-01 13:54:05 -0600

    split implementation into .cpp file where it should have been all along

--- src/interp.cpp
+++ src/interp.cpp
@@ -1,5 +1,89 @@
 #include "interp.hpp"
 
+Base_interp::Base_interp(const vector<Real> &x, const vector<Real> &y, const int m)
+  // constructor. set up for interpolating on a table of x's and y's
+  // of length m. Normally called by derived class, not user.
+  : n(x.size()), mmm(m), jsav(0), cor(0) {
+  if (n==0) {
+    xx = NULL;
+    yy = NULL;
+  } else {
+    xx = new Real[n];
+    yy = new Real[n];
+  }
+  for (int i=0;i<n;i++) {
+    xx[i]=x[i];
+    yy[i]=y[i];
+  }
+    
+  dj = std::min(1, (int) pow((Real) n, 0.25) );
+}
+
+Base_interp::Base_interp(const Base_interp &B) {
+  n=B.n;
+  mmm=B.mmm;
+  jsav=B.jsav;
+  cor=B.cor;
+  dj=B.dj;
+  if (n==0) {
+    xx = NULL;
+    yy = NULL;
+  } else {
+    xx = new Real[n];
+    yy = new Real[n];
+  }
+  for (int i=0; i<n; i++) {
+    xx[i]=B.xx[i];
+    yy[i]=B.yy[i];
+  }
+}
+
+Base_interp& Base_interp::operator= (const Base_interp &B) {
+  n=B.n;
+  mmm=B.mmm;
+  jsav=B.jsav;
+  cor=B.cor;
+  dj=B.dj;
+  if (n==0) {
+    xx = NULL;
+    yy = NULL;
+  } else {
+    xx = new Real[n];
+    yy = new Real[n];
+  }
+  for (int i=0; i<n; i++) {
+    xx[i]=B.xx[i];
+    yy[i]=B.yy[i];
+  }
+  return *this;
+}
+
+Base_interp::~Base_interp() {
+  if (n>0) {
+    delete [] xx;
+    delete [] yy;
+  }
+}
+
+Real Base_interp::operator()(const Real x) const 
+// Given a value x, return an interpolated value, using data
+// pointed to by xx and yy.
+{
+  int jlo = cor ? hunt(x) : locate (x);
+  return rawinterp(jlo, x);
+}
+
+int Base_interp::index(const Real x) const
+// Given a value x, return an interpolated value, using data
+// pointed to by xx and yy.
+{
+  int jlo = cor ? hunt(x) : locate(x);
+  return jlo;
+}
+
+
+
+
 int Base_interp::locate(const Real x) const
 // given a value x, return a value j such that x is (insofar as
 // possible) centered in the subrange xx[j..j+mmm-1], where xx is the
@@ -86,3 +170,37 @@ int Base_interp::hunt(const Real x) const
   jsav = jl;
   return std::max(0,std::min(n-mmm,jl-((mmm-2)>>1)));
 }
+
+Linear_interp::Linear_interp(const vector<Real> &xv, const vector<Real> &yv)
+  : Base_interp(xv,yv,2) {}
+
+
+
+Real Linear_interp::rawinterp(const int j, const Real x) const {
+  if(xx[j] == xx[j+1]) return yy[j]; // table defective, but recover
+  else return yy[j] + ((x-xx[j])/(xx[j+1]-xx[j]))*(yy[j+1]-yy[j]);
+}
+
+Bilinear_interp::Bilinear_interp(const vector<Real> &x1v,
+				 const vector<Real> &x2v,
+				 const MatrixX &ym)
+  : m(x1v.size()), n(x2v.size()), x1terp(x1v,x1v), x2terp(x2v,x2v) { y = ym; }
+
+Real Bilinear_interp::interp(const Real x1p, const Real x2p) const {
+  int i, j;
+  Real yy, t, u;
+  //find the grid square:
+  i = x1terp.cor ? x1terp.hunt(x1p) : x1terp.locate(x1p);
+  j = x2terp.cor ? x2terp.hunt(x2p) : x2terp.locate(x2p);
+  
+  //interpolate:
+  t = (x1p-x1terp.xx[i])/(x1terp.xx[i+1]-x1terp.xx[i]);
+  u = (x2p-x2terp.xx[j])/(x2terp.xx[j+1]-x2terp.xx[j]);
+  yy =  (1.-t)*(1.-u) * y(i  ,j  )
+    +    t *(1.-u) * y(i+1,j  )
+    +(1.-t)*    u  * y(i  ,j+1)
+    +    t *    u  * y(i+1,j+1);
+  
+  return yy;
+}
+
--- src/interp.hpp
+++ src/interp.hpp
@@ -19,89 +19,15 @@ struct Base_interp
   mutable int jsav, cor;
   Real *xx, *yy;
 
-  Base_interp() { }
-  
-  Base_interp(vector<Real> &x, vector<Real> &y, int m)
-  // constructor. set up for interpolating on a table of x's and y's
-  // of length m. Normally called by derived class, not user.
-    : n(x.size()), mmm(m), jsav(0), cor(0) {
-    if (n==0) {
-      xx = NULL;
-      yy = NULL;
-    } else {
-      xx = new Real[n];
-      yy = new Real[n];
-    }
-    for (int i=0;i<n;i++) {
-      xx[i]=x[i];
-      yy[i]=y[i];
-    }
-    
-    dj = std::min(1, (int) pow((Real) n, 0.25) );
-  }
+  Base_interp() : n(0) { }
+  Base_interp(const vector<Real> &x, const vector<Real> &y, const int m);
+  Base_interp(const Base_interp &B);
+  Base_interp& operator= (const Base_interp &B);
+  ~Base_interp();
 
-  Base_interp(const Base_interp &B) {
-    n=B.n;
-    mmm=B.mmm;
-    jsav=B.jsav;
-    cor=B.cor;
-    dj=B.dj;
-    if (n==0) {
-      xx = NULL;
-      yy = NULL;
-    } else {
-      xx = new Real[n];
-      yy = new Real[n];
-    }
-    for (int i=0; i<n; i++) {
-      xx[i]=B.xx[i];
-      yy[i]=B.yy[i];
-    }
-  }
+  Real operator()(const Real x) const;
 
-  Base_interp& operator= (const Base_interp &B) {
-    n=B.n;
-    mmm=B.mmm;
-    jsav=B.jsav;
-    cor=B.cor;
-    dj=B.dj;
-    if (n==0) {
-      xx = NULL;
-      yy = NULL;
-    } else {
-      xx = new Real[n];
-      yy = new Real[n];
-    }
-    for (int i=0; i<n; i++) {
-      xx[i]=B.xx[i];
-      yy[i]=B.yy[i];
-    }
-    return *this;
-  }
-
-  ~Base_interp() {
-    if (n>0) {
-      delete [] xx;
-      delete [] yy;
-    }
-  }
-
-
-  Real operator()(const Real x) const 
-  // Given a value x, return an interpolated value, using data
-  // pointed to by xx and yy.
-  {
-    int jlo = cor ? hunt(x) : locate (x);
-    return rawinterp(jlo, x);
-  }
-
-  int index(const Real x) const
-  // Given a value x, return an interpolated value, using data
-  // pointed to by xx and yy.
-  {
-    int jlo = cor ? hunt(x) : locate(x);
-    return jlo;
-  }
+  int index(const Real x) const;
 
   int locate(const Real x) const;
   int hunt(const Real x) const;
@@ -116,13 +42,9 @@ struct Linear_interp : Base_interp
 // vectors, then call interp for interpolated values
 {
   Linear_interp() : Base_interp() {}
-
-  Linear_interp(vector<Real> &xv, vector<Real> &yv) : Base_interp(xv,yv,2) {}
+  Linear_interp(const vector<Real> &xv, const vector<Real> &yv);
   
-  Real rawinterp(const int j, const Real x) const {
-    if(xx[j] == xx[j+1]) return yy[j]; // table defective, but recover
-    else return yy[j] + ((x-xx[j])/(xx[j+1]-xx[j]))*(yy[j+1]-yy[j]);
-  }
+  Real rawinterp(const int j, const Real x) const;
 };
 
 struct Bilinear_interp
@@ -133,32 +55,13 @@ struct Bilinear_interp
 {
   int m, n;
   MatrixX y;
+  // we need dummy 1 dim interp objects for their locate and hunt functions
   Linear_interp x1terp, x2terp;
 
   Bilinear_interp() { }
-  
-  Bilinear_interp(vector<Real> &x1v, vector<Real> &x2v, MatrixX &ym)
-    : m(x1v.size()), n(x2v.size()), x1terp(x1v,x1v), x2terp(x2v,x2v) { y = ym; }
-    // we need dummy 1 dim interp objects for their locate and hunt functions
+  Bilinear_interp(const vector<Real> &x1v, const vector<Real> &x2v, const MatrixX &ym);
 
-  Real interp(const Real x1p, const Real x2p) const {
-    int i, j;
-    Real yy, t, u;
-    //find the grid square:
-    i = x1terp.cor ? x1terp.hunt(x1p) : x1terp.locate(x1p);
-    j = x2terp.cor ? x2terp.hunt(x2p) : x2terp.locate(x2p);
-
-    //interpolate:
-    t = (x1p-x1terp.xx[i])/(x1terp.xx[i+1]-x1terp.xx[i]);
-    u = (x2p-x2terp.xx[j])/(x2terp.xx[j+1]-x2terp.xx[j]);
-    yy =  (1.-t)*(1.-u) * y(i  ,j  )
-	 +    t *(1.-u) * y(i+1,j  )
-	 +(1.-t)*    u  * y(i  ,j+1)
-	 +    t *    u  * y(i+1,j+1);
-
-    return yy;
-  }
-
+  Real interp(const Real x1p, const Real x2p) const;
 };
 
 #endif
