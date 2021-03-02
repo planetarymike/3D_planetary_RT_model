8e100ebd6e493ffd6d885475eca5431c7ce90a72
Mike <michael.s.chaffin@gmail.com>
2021-03-02 13:22:41 -0700

    PEP8 updates

--- python/setup_corona_sim.py
+++ python/setup_corona_sim.py
@@ -1,22 +1,28 @@
-from setuptools import setup, Extension
-from setuptools.command import build_ext
-from Cython.Build import cythonize
-from nvcc_integration import *
 import sys
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+import numpy
+
+from nvcc_integration import locate_cuda
+
+boost_dir = "/home/mike/Documents/Utilities/boost_1_73_0/"
+eigen_dir = "/home/mike/Documents/Utilities/eigen-3.3.7/"
 
 extension = Extension("py_corona_sim",
                       ["py_corona_sim.pyx"],
-                      extra_objects=['build/libobservation_fit.a','../bin/ipbackgroundCFR_fun.o'],
+                      extra_objects=['build/libobservation_fit.a',
+                                     '../bin/ipbackgroundCFR_fun.o'],
                       language="c++",
-                      extra_link_args=['-lgfortran'],
-                      extra_compile_args=['-lgfortran','-lm','-fopenmp','-O3','-march=native',
-                                          '-DNDEBUG','-fPIC'],
+                      extra_link_args=['-lgfortran', '-lgfortran', '-lm'],
+                      extra_compile_args=['-fopenmp', '-O3', '-march=native',
+                                          '-DNDEBUG', '-fPIC'],
                       include_dirs=[numpy.get_include(),
                                     "../src/",
-                                    "/home/mike/Documents/Utilities/boost_1_73_0/",
-                                    "/home/mike/Documents/Utilities/eigen-3.3.7/"])
+                                    boost_dir,
+                                    eigen_dir])
 
-#relies on command line -D RT_FLOAT to compile GPU code
+# relies on command line -D RT_FLOAT to compile GPU code
 if "-RT_FLOAT" in sys.argv:
     print("compiling with Real=float")
 
@@ -30,16 +36,17 @@ if "-RT_FLOAT" in sys.argv:
     extension.runtime_library_dirs.append(CUDA['lib64'])
     extension.include_dirs.append(CUDA['include'])
 
-    cython_compile_time={'RT_FLOAT':True}
+    cython_compile_time = {'RT_FLOAT': True}
 
     sys.argv.remove("-RT_FLOAT")
 else:
     print("compiling with Real=double")
-    cython_compile_time={'RT_FLOAT':False}
+    cython_compile_time = {'RT_FLOAT': False}
 
 setup(name="py_corona_sim",
-      author = "Mike Chaffin",
-      version = "0.0",
-      ext_modules = cythonize([extension], compile_time_env=cython_compile_time),
-      zip_safe = False
+      author="Mike Chaffin",
+      version="0.0",
+      ext_modules=cythonize([extension],
+                            compile_time_env=cython_compile_time),
+      zip_safe=False
       )
